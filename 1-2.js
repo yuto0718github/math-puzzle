6b9be0bb18e306b7536dd140f688c925fd5e0ef1
YutosCodes <yuto.y.0718@gmail.com>
2025-02-24 12:35:06 +0900

    Add script to evaluate mathematical expressions for four-digit numbers

---
+++ 1-2.js
@@ -0,0 +1,18 @@
+
+const op = ["+", "-", "*", "/", ""];
+for ( i = 1000; i < 10000; i++) {
+    const c = String(i);
+    for (j = 0; j < op.length; j++) {
+        for (k = 0; k < op.length; k++) {
+            for (l = 0; l < op.length; l++) {
+                val = c.charAt(3) + op[j] + c.charAt(2) + op[k] + c.charAt(1) + op[l] + c.charAt(0);
+                if (val.length > 4) {
+                    if (i == eval(val)) {
+                        console.log(val + " = " + i);
+                    }
+                }
+            }
+        }
+    }
+}
+
